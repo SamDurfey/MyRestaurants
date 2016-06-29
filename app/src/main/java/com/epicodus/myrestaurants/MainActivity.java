c649ca6f582f2d62c6e3e955506de8bb315c7782
SamDurfey <samuel.durfey@gmail.com>
2016-06-29 00:31:58 -0700

    Caught up through Monday Homework:
    Created Restaurants Activity, adapter working with list view. Entered string is transferred to new page with ButterKnife.

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,13 +1,37 @@
 package com.epicodus.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.restaurantsButton) Button mRestaurantsButton;
+    @Bind(R.id.locationEntry) EditText mLocationEntry;
+    @Bind(R.id.appNameTextView) TextView mTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Typeface stuff
+
+        mRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String location = mLocationEntry.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
     }
 }
